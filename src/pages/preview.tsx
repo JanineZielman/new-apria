6fc59223878fc384d5c92d22fcdb54197b8cc1b7
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-04-11 17:06:01 +0200

    update

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -7,28 +7,26 @@ export default function Preview() {
   const isLoading = client.useIsLoading();
   const { typeName, node } = client.auth.usePreviewNode();
 
-  console.log(node)
+  if (isLoading || node === undefined) {
+    return <p>Loading...</p>;
+  }
 
-  // if (isLoading || node === undefined) {
-  //   return <p>Loading...</p>;
-  // }
+  if (node === null) {
+    return <p>Post not found</p>;
+  }
 
-  // if (node === null) {
-  //   return <p>Post not found</p>;
-  // }
-
-  // switch (typeName) {
-  //   case 'Page': {
-  //     const page = node as Page;
-  //     return <PageComponent page={page} />;
-  //   }
-  //   case 'Post': {
-  //     const post = node as Post;
-  //     return <PostComponent post={post} />;
-  //   }
-  //   // Add custom post types here as needed
-  //   default: {
-  //     throw new Error(`Unknown post type: ${typeName}`);
-  //   }
-  // }
+  switch (typeName) {
+    case 'Page': {
+      const page = node as Page;
+      return <PageComponent page={page} />;
+    }
+    case 'Post': {
+      const post = node as Post;
+      return <PostComponent post={post} />;
+    }
+    // Add custom post types here as needed
+    default: {
+      throw new Error(`Unknown post type: ${typeName}`);
+    }
+  }
 }
